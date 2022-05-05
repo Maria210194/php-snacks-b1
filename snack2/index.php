2313363e1f1496c325439214224d0f51d3cb3718
Maria Stachie <mary.stachie@gmail.com>
2022-05-05 09:25:49 +0200

    snack2 done

--- snack2/index.php
+++ snack2/index.php
@@ -1,3 +1,24 @@
 <?php
 
+
+if(!empty($_GET)){
+    if(!isset($_GET['name']) || strlen($_GET['name']) <= 3 ){
+        echo('Il nome deve contenere almeno 3 caratteri');
+    }elseif(!isset($_GET['mail']) || strpos($_GET['mail'], '@' )==false || strpos($_GET['mail'], '.' )==false){
+        echo('La mail non contiene @ oppure un punto');
+    }elseif(!isset($_GET['age']) || is_numeric($_GET['age'])==false){
+        echo('L\'età inserita non è un numero');
+    }else {
+        echo('Accesso riuscito');
+    }
+}
+
 ?>
+
+
+<form action="index.php" method="get">
+    <input type="text" name="name" />
+    <input type="text" name="mail" />
+    <input type="number" name="age" />
+    <input type="submit" />
+</form>
